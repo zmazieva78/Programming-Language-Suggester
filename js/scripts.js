87885ee3bfcff3cb3c611cba287d843305f1fa60
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-25 14:22:36 -0700

    add if else statemtnts with four different combinations 11 12 21 22 and four programming languages

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
-
 $(function() {
   $("#modalSubmit").click(function() {
-// Name input in module form
+// Name input in modal form
     const nameInput = $("#name").val();
-
-    const answer1 = $('input[name=question1]:checked').val();
-    const answer2 = $('input[name=question2]:checked').val();
-    const answer3 = $('input[name=question3]:checked').val();
-    const answer4 = $('input[name=question4]:checked').val();
-    const answer5 = $('input[name=question5]:checked').val();
-
+    const answer1 = parseInt($('input[name=question1]:checked').val());
+    const answer2 = parseInt($('input[name=question2]:checked').val());
+    const answer3 = parseInt($('input[name=question3]:checked').val());
+    const answer4 = parseInt($('input[name=question4]:checked').val());
+    const answer5 = parseInt($('input[name=question5]:checked').val());
     console.log(nameInput,answer1, answer2, answer3, answer4, answer5);
 
+    if (answer1 == "1" && answer2 == "1") {
+      $("#quizerResult").text("C#");
+      $("#quizerName").text(nameInput);
+      $(".result").show();
+    } else if (answer1 == "1" && answer2 == "2") {
+      $("#quizerResult").text("Python");
+      $("#quizerName").text(nameInput);
+      $(".result").show();
+    } else if (answer1 == "2" && answer2 == "1") {
+      $("#quizerResult").text("JavaScript");
+      $("#quizerName").text(nameInput);
+      $(".result").show();
+    } else if (answer1 == "2" && answer2 == "2") {
+      $("#quizerResult").text("Rust");
+      $("#quizerName").text(nameInput);
+      $(".result").show();
+    }
   });
 });
