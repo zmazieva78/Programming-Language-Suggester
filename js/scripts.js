e392ec4a29f730fc46339c66a977c3720114133a
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-25 13:28:01 -0700

    all value is stored inside click(function)

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 
 $(function() {
-  var nameInput = "";
-  var results = "";
-
   $("#modalSubmit").click(function() {
 // Name input in module form
-    nameInput = $("#name").val();
-//This function is working, checked
- 
+    const nameInput = $("#name").val();
 
+    const answer1 = $('input[name=question1]:checked').val();
+    const answer2 = $('input[name=question2]:checked').val();
+    const answer3 = $('input[name=question3]:checked').val();
+    const answer4 = $('input[name=question4]:checked').val();
+    const answer5 = $('input[name=question5]:checked').val();
 
-
-
-
-
+    console.log(nameInput,answer1, answer2, answer3, answer4, answer5);
 
   });
 });
