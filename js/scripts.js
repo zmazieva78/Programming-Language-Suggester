8ed2cf17646cd7f1011afd07e0d441d7caeaa4cd
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-25 16:37:26 -0700

    add alert to name input and returns false

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(function() {
   $("#modalSubmit").click(function() {
 // Name input and radio buttons input stored in CONST in modal form
     const nameInput = $("#name").val();
+    if (nameInput.trim() == "") {
+      $("#alert").show();
+      return false;
+    };
     const answer1 = parseInt($('input[name=question1]:checked').val());
     const answer2 = parseInt($('input[name=question2]:checked').val());
     const answer3 = parseInt($('input[name=question3]:checked').val());
@@ -12,6 +16,8 @@ $(function() {
 // Five questions create a lot of combinations and to list all of them would be too long for each statement,
   // I decided to go with first two numbers for each Programming language result by selecting only 11 12 21 22
    // and assigned results to four different languages
+
+
     if (answer1 == "1" && answer2 == "1") {
       $("#quizerResult").text("C#");
       $("#quizerName").text(nameInput);
@@ -28,6 +34,7 @@ $(function() {
       $("#quizerResult").text("Rust");
       $("#quizerName").text(nameInput);
       $(".result").slideDown();
-    }
+    };
+
   });
 });
